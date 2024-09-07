a6d7c1a7f9bad0a3cc16aba33ebd80de54150106
Harshueri01803 <harshitvdsri7469@gmail.com>
2024-09-07 22:27:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>HARSHIT SRIVASTAVA SERVER 2024</title>
+  <title>HARSHIT  SERVER 2024</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     body{
-      background-color: Black;
+      background-color: white;
     }
     .container{
       max-width: 300px;
